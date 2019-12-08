9b808508c8fdad56c62194b57d2ba1f12bdaaa75
ricklinpk <rickl2@illinois.edu>
2019-12-08 14:43:05 -0600

    constants

---
+++ app/src/main/java/com/example/finalproject/Constants.java
@@ -0,0 +1,17 @@
+package com.example.finalproject;
+/*
+ * A handful of classes that hold numeric constants involved in server communication (Checkpoint 4).
+ * STOP! Do not modify this file. Changes will be overwritten during official grading.
+ */
+
+/**
+ * Holds the constant values for the possible game states.
+ */
+public class Constants {
+    static final int FOOD  = 0;
+    static final int TRANSPORTATION = 1;
+    static final int DRINK = 2;
+    static final int ELECTRONICS  = 3;
+    static final int ENTERTAINMENT = 4;
+    static final int OTHERS = 5;
+}
